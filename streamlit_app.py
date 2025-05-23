12463810d075b8fc5b5644215bbcec822643be70
nnaevaaurelia <naeva.aureliazahra@gmail.com>
2025-05-23 07:55:30 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,6 @@ import streamlit as st
 st.title("Selamat Datang di Web Informatika")
 st.write(
     "ngoding seru bersama Bapak Hendri"
+)
+st.image("https://github.com/nnaevaaurelia/LatihanInforNaeva/blob/main/IMG_2260.png", width=200)
 
